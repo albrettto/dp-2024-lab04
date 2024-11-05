f2f30380f2bedd0df9c07ce9bee8b154bae36cd4
Хусамов Альберт <a.husamov@yandex.ru>
2024-11-06 01:50:58 +0500

    docs(analog_clock): Добавлена документация для аналоговых часов

--- clocks/analog_clock.py
+++ clocks/analog_clock.py
@@ -22,6 +22,17 @@ class AnalogClock(BaseAnalogClock):
         second_angle: float,
         day_night_division: DayNightDivision,
     ):
+        """
+        Устанавливает текущую дату
+
+        :param year: год
+        :param month: месяц
+        :param day: день
+        :param hour_angle: угол стрелки часов
+        :param minute_angle: угол минутной стрелки
+        :param second_angle: угол секундной стрелки
+        :param day_night_division: текущее время суток (день / ночь)
+        """
         self.year = year
         self.month = month
         self.day = day
@@ -31,22 +42,43 @@ class AnalogClock(BaseAnalogClock):
         self.day_night_division = day_night_division
 
     def get_hour_angle(self) -> float:
+        """
+        Возвращает текущий угол стрелки часов
+        """
         return self.hour_angle
 
     def get_minute_angle(self) -> float:
+        """
+        Возвращает текущий угол минутной стрелки
+        """
         return self.minute_angle
 
     def get_second_angle(self) -> float:
+        """
+        Возвращает текущий угол секундной стрелки
+        """
         return self.second_angle
 
     def get_year(self) -> int:
+        """
+        Возвращает текущий год (от установленного пользователем)
+        """
         return self.year
 
     def get_month(self) -> int:
+        """
+        Возвращает текущий месяц (от установленного пользователем)
+        """
         return self.month
 
     def get_day(self) -> int:
+        """
+        Возвращает текущий день (от установленного пользователем)
+        """
         return self.day
 
     def get_day_night_division(self) -> DayNightDivision:
+        """
+        Возвращает текущее время суток (день / ночь)
+        """
         return self.day_night_division
